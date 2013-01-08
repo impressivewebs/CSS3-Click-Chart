cde8385fed01dd34d432e8e256cf27c0dd0afa1b
Louis Lazaris <lazaris@impressivewebs.com>
2013-01-07 19:03:47 -0500

    fixed site css

--- includes/features/background-clip/code.php
+++ includes/features/background-clip/code.php
@@ -3,7 +3,6 @@
     padding: 30px;	
     -webkit-background-clip: content-box;
     -moz-background-clip: content-box;
-    -o-background-clip: content-box;
     -ms-background-clip: content-box;
     background-clip: content-box; /* border-box, padding-box */
 }
