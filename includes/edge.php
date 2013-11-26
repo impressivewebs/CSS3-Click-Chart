91e3f61bace8589f7bbf170f4994d56d112ac774
Louis Lazaris <lazaris@impressivewebs.com>
2013-11-26 08:08:37 -0500

    fixed indenting

--- includes/edge.php
+++ includes/edge.php
@@ -1,34 +1,34 @@
 <li class="colg"><a href="?prop=calc">calc()</a></li>
-            <li class="colg"><a href="?prop=regions">regions</a></li>
-            <li class="colg"><a href="?prop=flexbox">flexbox</a></li>
-            <li class="colg"><a href="?prop=masks">masks *</a></li>
-            <li class="colg"><a href="?prop=tab-size">tab size</a></li>
-            <li class="colg"><a href="?prop=toggle">toggle()</a></li>
+<li class="colg"><a href="?prop=regions">regions</a></li>
+<li class="colg"><a href="?prop=flexbox">flexbox</a></li>
+<li class="colg"><a href="?prop=masks">masks *</a></li>
+<li class="colg"><a href="?prop=tab-size">tab size</a></li>
+<li class="colg"><a href="?prop=toggle">toggle()</a></li>
 
-            <li class="colh coll"><a href="?prop=paged-media">paged media</a></li>
-            <li class="colh"><a href="?prop=3d-transform">3d transforms</a></li>
-            <li class="colh"><a href="?prop=grid-layout">grid layout</a></li>
-            <li class="colh"><a href="?prop=pointer-events">pointer events</a></li>
-            <li class="colh"><a href="?prop=text-overflow">text overflow</a></li>
-            <li class="colh"><a href="?prop=conditional-supports">@supports</a></li>
+<li class="colh coll"><a href="?prop=paged-media">paged media</a></li>
+<li class="colh"><a href="?prop=3d-transform">3d transforms</a></li>
+<li class="colh"><a href="?prop=grid-layout">grid layout</a></li>
+<li class="colh"><a href="?prop=pointer-events">pointer events</a></li>
+<li class="colh"><a href="?prop=text-overflow">text overflow</a></li>
+<li class="colh"><a href="?prop=conditional-supports">@supports</a></li>
 
-            <li class="coli coll"><a href="?prop=namespace">@namespace</a></li>
-            <li class="coli"><a href="?prop=cursors">new cursors</a></li>
-            <li class="coli"><a href="?prop=hyphenation">hyphenation</a></li>
-            <li class="coli"><a href="?prop=exclusions">exclusions</a></li>
-            <li class="coli"><a href="?prop=currentcolor">currentColor</a></li>
-            <li class="coli"><a href="?prop=box-alignment">box alignment</a></li>
+<li class="coli coll"><a href="?prop=namespace">@namespace</a></li>
+<li class="coli"><a href="?prop=cursors">new cursors</a></li>
+<li class="coli"><a href="?prop=hyphenation">hyphenation</a></li>
+<li class="coli"><a href="?prop=exclusions">exclusions</a></li>
+<li class="coli"><a href="?prop=currentcolor">currentColor</a></li>
+<li class="coli"><a href="?prop=box-alignment">box alignment</a></li>
 
-            <li class="colj coll"><a href="?prop=filters">filters</a></li>
-            <li class="colj"><a href="?prop=cssom">cssom</a></li>
-            <li class="colj"><a href="?prop=marquee">marquee</a></li>
-            <li class="colj"><a href="?prop=rem-units">rem units</a></li>
-            <li class="colj"><a href="?prop=viewport">@viewport</a></li>
-            <li class="colj"><a href="?prop=variables">variables</a></li>
+<li class="colj coll"><a href="?prop=filters">filters</a></li>
+<li class="colj"><a href="?prop=cssom">cssom</a></li>
+<li class="colj"><a href="?prop=marquee">marquee</a></li>
+<li class="colj"><a href="?prop=rem-units">rem units</a></li>
+<li class="colj"><a href="?prop=viewport">@viewport</a></li>
+<li class="colj"><a href="?prop=variables">variables</a></li>
 
-            <li class="colk coll"><a href="?prop=overflow-wrap">overflow wrap</a></li>
-            <li class="colk"><a href="?prop=blending-modes">blending modes</a></li>
-            <li class="colk"><a href="?prop=canvas-backgrounds">canvas backgrounds *</a></li>
-            <li class="colk"><a href="?prop=template-layout">template layout</a></li>
-            <li class="colk"><a href="?prop=bg-local">bg-attachment: local</a></li>
-            <li class="colk"><a href="?prop=object-fit-position">object-fit/position</a></li>
+<li class="colk coll"><a href="?prop=overflow-wrap">overflow wrap</a></li>
+<li class="colk"><a href="?prop=blending-modes">blending modes</a></li>
+<li class="colk"><a href="?prop=canvas-backgrounds">canvas backgrounds *</a></li>
+<li class="colk"><a href="?prop=template-layout">template layout</a></li>
+<li class="colk"><a href="?prop=bg-local">bg-attachment: local</a></li>
+<li class="colk"><a href="?prop=object-fit-position">object-fit/position</a></li>
