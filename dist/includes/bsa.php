e1633ebb9bf4ce831594f105a561025bcb5ed84a
Louis Lazaris <lazaris@impressivewebs.com>
2017-09-07 06:53:03 -0400

    Fixed old ad code / SSL

--- dist/includes/bsa.php
+++ dist/includes/bsa.php
@@ -5,30 +5,69 @@
 <head>
 <style>
 * {
-    margin: 0;
-    padding: 0;
+  margin: 0;
+  padding: 0;
 }
 
-    .bsa_it_ad a {
-        display: block;
-    }
-    
-    .bsa_it_p a {
-        display: none;
-    }
-    
-    .extbsa {
-        font-size: 10px;
-        color: #666;
-        text-decoration: none;
-        position: absolute;
-        bottom: 6px;
-        right: 14px;
-        z-index: 10;
-    }
+  .carbon-img {
+    float: left;
+    margin-right: 10px;
+  }
+
+  .bsa_it_ad a {
+    display: block;
+  }
+  
+  .bsa_it_p a {
+    display: none;
+  }
+  
+  .extbsa {
+    font-size: 10px;
+    color: #666;
+    text-decoration: none;
+    position: absolute;
+    bottom: 6px;
+    right: 14px;
+    z-index: 10;
+  }
+
+  span.carbon-wrap {
+    font-family: Arial, sans-serif;
+    font-size: 14px;
+  }
+
+  div#carbonads {
+    background: white;
+    overflow: hidden;
+    padding: 10px;
+  }
+
+  .carbon-img {
+    float: left;
+    margin-right: 10px;
+  }
+
+  a.carbon-text {
+    color: black;
+    text-decoration: none;
+  }
+
+  a.carbon-poweredby {
+    color: black;
+    font-size: 12px;
+    display: block;
+    margin-top: 27px;
+    margin-right: 0px;
+    margin-bottom: 10px;
+    font-family: Verdana, sans-serif;
+    float: right;
+  }
 </style>
 </head>
 <body>
-<script type="text/javascript" src="http://cdn.adpacks.com/adpacks.js?legacyid=1277044&zoneid=1386&key=c39a163a3eecd8aadac62be710ab632f&serve=C6SI42Y&placement=css3clickchartcom&circle=dev" id="_adpacks_js"></script>
+<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=css3clickchartcom" id="_carbonads_js"></script>
+
+<!--<script type="text/javascript" src="http://cdn.adpacks.com/adpacks.js?legacyid=1277044&zoneid=1386&key=c39a163a3eecd8aadac62be710ab632f&serve=C6SI42Y&placement=css3clickchartcom&circle=dev" id="_adpacks_js"></script>-->
 </body>
 </html>
